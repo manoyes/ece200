d167097fcc7ef767d7c80ed1823557cf50ce9bad
Matthew Noyes <matt@dhcp-10-5-4-122.wireless.rochester.edu>
2014-03-03 15:16:44 -0500

    Implemented Structural Comparator
    Added implementation of an 8-bit structural comparator. Outputs 0 if A
    = B, 1 if A > B, 2 if A < B.

--- lab2/comparator_structural.v
+++ lab2/comparator_structural.v
@@ -5,8 +5,27 @@ input [7:0] A, B;
 
 // ===== OUTPUTS =====
 output [1:0] C;
+wire[7:0] eq;
 
-assign C[0] = (A & ~B);
-assign C[1] = (~A & B);
+assign eq = (A & B) | (~A & ~B);
 
+// (A > B)
+assign C[0] = (A[7] & ~B[7]) |
+              (A[6] & ~B[6] & eq[7]) |
+              (A[5] & ~B[5] & eq[7] & eq[6]) |
+              (A[4] & ~B[4] & eq[7] & eq[6] & eq[5]) |
+              (A[3] & ~B[3] & eq[7] & eq[6] & eq[5] & eq[4]) |
+              (A[2] & ~B[2] & eq[7] & eq[6] & eq[5] & eq[4] & eq[3]) |
+              (A[1] & ~B[1] & eq[7] & eq[6] & eq[5] & eq[4] & eq[3] & eq[2]) |
+              (A[0] & ~B[0] & eq[7] & eq[6] & eq[5] & eq[4] & eq[3] & eq[2] & eq[1]);
+                            
+// (A < B)
+assign C[1] = (~A[7] & B[7]) |
+              (~A[6] & B[6] & eq[7]) |
+              (~A[5] & B[5] & eq[7] & eq[6]) |
+              (~A[4] & B[4] & eq[7] & eq[6] & eq[5]) |
+              (~A[3] & B[3] & eq[7] & eq[6] & eq[5] & eq[4]) |
+              (~A[2] & B[2] & eq[7] & eq[6] & eq[5] & eq[4] & eq[3]) |
+              (~A[1] & B[1] & eq[7] & eq[6] & eq[5] & eq[4] & eq[3] & eq[2]) |
+              (~A[0] & B[0] & eq[7] & eq[6] & eq[5] & eq[4] & eq[3] & eq[2] & eq[1]);
 endmodule
