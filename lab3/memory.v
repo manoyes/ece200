953040466638def604e87eb4df4217cca3eea495
Matthew Noyes <manoyes.github@gmail.com>
2014-04-07 23:02:11 -0400

    Re-added reset functionality.

--- lab3/memory.v
+++ lab3/memory.v
@@ -22,6 +22,24 @@ reg [15:0] datastore[15:0]; // Memory storage
   always @ (negedge clk) begin    
     
     datastore[4'b0000] <= 16'b0000000000000000;
+    
+    if (rst) begin
+      datastore[1] <= 16'b0000000000000000;
+      datastore[2] <= 16'b0000000000000000;
+      datastore[3] <= 16'b0000000000000000;
+      datastore[4] <= 16'b0000000000000000;
+      datastore[5] <= 16'b0000000000000000;
+      datastore[6] <= 16'b0000000000000000;
+      datastore[7] <= 16'b0000000000000000;
+      datastore[8] <= 16'b0000000000000000;            
+      datastore[9] <= 16'b0000000000000000;
+      datastore[10] <= 16'b0000000000000000;
+      datastore[11] <= 16'b0000000000000000;
+      datastore[12] <= 16'b0000000000000000;
+      datastore[13] <= 16'b0000000000000000;
+      datastore[14] <= 16'b0000000000000000;
+      datastore[15] <= 16'b0000000000000000;
+    end
         
     if (WriteRgAddr > 4'b0000) // R0 is hardwired to 0, writing to it discards the value.
         datastore[WriteRgAddr] = WriteData; 
